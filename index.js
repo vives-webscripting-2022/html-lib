ecee0ce776ea5caa38a19ae46808308ede01b1e8
Sille Van Landschoot <info@sillevl.be>
2022-10-12 09:30:49 +0200

    refactor to ES6 modules and implementation of HTTP server

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
-const { Heading , P } = require('./lib/HtmlLib')
+import { Heading, P } from './lib/HtmlLib.js'
+import chalk from 'chalk'
+import net from 'net'
+import { connect } from 'http2'
+
+const PORT = 80
 
 const document = [ 
     new Heading('Hello World', 1),
@@ -8,4 +13,42 @@ const document = [
     new Heading('subtitle', 2)
 ]
 
-console.log(document.join(''))
+const server = net.createServer()
+
+server.listen(PORT, () => {
+    console.log(chalk.bgBlue(`Listening on port ${PORT}...`))
+})
+
+const sendResponse = (connection, response) => {
+    const { protocol, statusCode, statusMessage, body} = response
+    const data = `${protocol} ${statusCode} ${statusMessage}\r\n\r\n${body}`
+    connection.write(data)
+    connection.end()
+}
+
+server.on('connection', (connection) => {
+    console.log("New connection established!")
+    connection.on('data', (data) => {
+        const lines = data.toString().split('\r\n')
+
+        const [ method ,path, protocol ] = lines[0].split(' ')
+        const request = { method, path, protocol }
+        console.log(request)
+
+        const response = {
+            protocol: "HTTP/1.1",
+            statusCode: 400,
+            statusMessage: 'document not found',
+            body: 'document not found 404'
+        }
+        
+        if(request.path === '/') {
+            response.statusCode = 200
+            response.statusMessage = 'OK'
+            response.body = document.join('')
+        } 
+
+        sendResponse(connection, response)
+    })
+} )
+
